6325bc1ef9c9c4aa032e1d2f9271e1bdd601e347
Lehahiah Angels <135294947+LehahiahAngels@users.noreply.github.com>
2023-06-08 02:02:25 +0100

    Dépot 2 avec quelques modifications

--- js/Index.js
+++ js/Index.js
@@ -1,13 +1,70 @@
-const menuHamburger = document.querySelector(".menu-hamburger")
-const navLinks = document.querySelector(".nav-links")
+const menuHamburger = document.querySelector(".menu-hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-menuHamburger.addEventListener('click',()=>{
-    navLinks.classList.toggle('mobile-menu');
-    if (menuHamburger.classList.contains("fa-bars")) {
-        menuHamburger.classList.replace("fa-bars", "fa-times");
-      } else if (menuHamburger.classList.contains("fa-times")) {
-        menuHamburger.classList.replace("fa-times", "fa-bars");
-      }
-})
+menuHamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("mobile-menu");
+  if (menuHamburger.classList.contains("fa-bars")) {
+    menuHamburger.classList.replace("fa-bars", "fa-times");
+  } else if (menuHamburger.classList.contains("fa-times")) {
+    menuHamburger.classList.replace("fa-times", "fa-bars");
+  }
+});
 
-  
+function carousel() {
+  let carouselSlider = document.querySelector(".carousel__slider");
+  let list = document.querySelector(".carousel__list");
+  let item = document.querySelectorAll(".carousel__item");
+  let list2;
+
+  const speed = 1;
+
+  const width = list.offsetWidth;
+  let x = 0;
+  let x2 = width;
+
+  function clone() {
+    list2 = list.cloneNode(true);
+    carouselSlider.appendChild(list2);
+    list2.style.left = `${width}px`;
+  }
+
+  function moveFirst() {
+    x -= speed;
+
+    if (width >= Math.abs(x)) {
+      list.style.left = `${x}px`;
+    } else {
+      x = width;
+    }
+  }
+
+  function moveSecond() {
+    x2 -= speed;
+
+    if (list2.offsetWidth >= Math.abs(x2)) {
+      list2.style.left = `${x2}px`;
+    } else {
+      x2 = width;
+    }
+  }
+
+  function hover() {
+    clearInterval(a);
+    clearInterval(b);
+  }
+
+  function unhover() {
+    a = setInterval(moveFirst, 10);
+    b = setInterval(moveSecond, 10);
+  }
+
+  clone();
+
+  let a = setInterval(moveFirst, 10);
+  let b = setInterval(moveSecond, 10);
+
+  carouselSlider.addEventListener("mouseenter", hover);
+  carouselSlider.addEventListener("mouseleave", unhover);
+}
+
+carousel();
