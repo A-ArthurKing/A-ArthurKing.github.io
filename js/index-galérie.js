96b3b2a83042db0c10fd3fdbc9082e79806a795d
Willix Digital <120456964+Willix-Digital@users.noreply.github.com>
2024-05-19 00:18:13 +0100

    Dépôt 25

---
+++ js/index-galérie.js
@@ -0,0 +1,19 @@
+/*!
+=================================================================================================================================
+     *Responsive Nav Bar
+=================================================================================================================================
+*/
+const navbarMenu = document.querySelector(".navbar .links");
+const hamburgerBtn = document.querySelector(".hamburger-btn");
+const hideMenuBtn = navbarMenu.querySelector(".close-btn");
+const showPopupBtn = document.querySelector(".login-btn");
+
+// Show mobile menu
+hamburgerBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("show-menu");
+});
+
+// Hide mobile menu
+hideMenuBtn.addEventListener("click", () => hamburgerBtn.click());
+
+
